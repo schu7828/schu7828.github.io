5dcc26837ddc953c01a3737448f46c22dca230ec
schu7828 <schu7828@users.noreply.github.com>
2025-09-24 19:21:00 -0500

    Style updates

---
+++ style.js
@@ -0,0 +1,24 @@
+function changeDivColor() {
+    const p = document.getElementById("Home1");
+    p.style.backgroundColor = "lightblue";
+  }
+
+function selectHomeTab() {
+     const p = document.getElementById("HomePageTab");
+     p.style.backgroundColor = "grey";
+}
+
+function selectProjectsTab() {
+    const p = document.getElementById("ProjectsTab");
+    p.style.backgroundColor = "grey";
+}
+
+function selectResumeTab() {
+    const p = document.getElementById("ResumeTab");
+    p.style.backgroundColor = "grey";
+}
+
+function selectFunTab() {
+    const p = document.getElementById("FunTab");
+    p.style.backgroundColor = "grey";
+}
